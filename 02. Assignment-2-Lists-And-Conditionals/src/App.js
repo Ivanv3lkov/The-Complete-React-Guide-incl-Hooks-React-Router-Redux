9c692be3c15b9c276a259f795755b1ca36bf25c2
ivanv3lkov <ivanv3lkov@gmail.com>
2020-12-01 22:06:41 +0200

    Assignment

--- 02. Assignment-2-Lists-And-Conditionals/src/App.js
+++ 02. Assignment-2-Lists-And-Conditionals/src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import './App.css';
 import Validation from './components/Validation'
 import Char from './components/Char'
@@ -9,27 +9,27 @@ class App extends Component {
   }
 
   inputChangedHandler = (event) => {
-    this.setState({userInput: event.target.value})
+    this.setState({ userInput: event.target.value })
   }
 
   deleteCharHandler = (index) => {
     const text = this.state.userInput.split('');
     text.splice(index, 1);
     const updatedText = text.join('');
-    this.setState({userInput: updatedText})
+    this.setState({ userInput: updatedText })
   }
-  
+
   render() {
     const charList = this.state.userInput.split('').map((char, index) => {
-      return <Char character={char} key={index} clicked={() => this.deleteCharHandler(index)}/>
+      return <Char character={char} key={index} clicked={() => this.deleteCharHandler(index)} />
     })
 
     return (
       <div className="App">
         <input
-        type="text"
-        onChange={this.inputChangedHandler}
-        value={this.state.userInput} />
+          type="text"
+          onChange={this.inputChangedHandler}
+          value={this.state.userInput} />
         <p>{this.state.userInput}</p>
         <Validation inputLength={this.state.userInput.length} />
         {charList}
