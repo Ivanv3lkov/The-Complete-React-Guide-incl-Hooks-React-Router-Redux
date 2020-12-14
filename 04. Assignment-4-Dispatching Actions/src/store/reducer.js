84f4dd8bf34515c12c6e19029b1bbb254b23899e
Ivan Velkov <ivanv3lkov@gmail.com>
2020-12-14 09:59:12 +0200

    Assignment

--- 04. Assignment-4-Dispatching Actions/src/store/reducer.js
+++ 04. Assignment-4-Dispatching Actions/src/store/reducer.js
@@ -3,24 +3,26 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) => {
-    if (action.type === 'INCREMENT') {
-        return {
-            counter: state.counter + 1
-        }
-    } else if (action.type === 'DECREMENT') {
-        return {
-            counter: state.counter - 1
-        }
-    } else if (action.type === 'ADD') {
-        return {
-            counter: state.counter + 5
-        }
-    } else if (action.type === 'SUBTRACT') {
-        return {
-            counter: state.counter - 8
-        }
+    switch (action.type) {
+        case 'INCREMENT':
+            return {
+                counter: state.counter + 1
+            }
+        case 'DECREMENT':
+            return {
+                counter: state.counter - 1
+            }
+        case 'ADD':
+            return {
+                counter: state.counter + 5
+            }
+        case 'SUBTRACT':
+            return {
+                counter: state.counter - 8
+            }
+        default:
+            return state
     }
-    return state;
-};
+}
 
 export default reducer;
