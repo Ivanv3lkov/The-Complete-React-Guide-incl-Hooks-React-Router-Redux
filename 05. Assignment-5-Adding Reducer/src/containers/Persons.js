f0eb10722b3181afcf9da244c68337403dfd4bfe
Ivan Velkov <ivanv3lkov@gmail.com>
2020-12-15 12:11:01 +0200

    Assignment 5

---
+++ 05. Assignment-5-Adding Reducer/src/containers/Persons.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import Person from '../components/Person/Person';
+import AddPerson from '../components/AddPerson/AddPerson';
+import * as actionTypes from '../store/actionTypes';
+
+class Persons extends Component {
+    render () {
+        return (
+            <div>
+                <AddPerson personAdded={this.props.onAddedPerson} />
+                {this.props.prs.map(person => (
+                    <Person 
+                        key={person.id}
+                        name={person.name} 
+                        age={person.age} 
+                        clicked={() => this.props.onRemovedPerson(person.id)}/>
+                ))}
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        prs: state.persons
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onAddedPerson: () => dispatch({type: actionTypes.ADD_PERSON}),
+        onRemovedPerson: (id) => dispatch({type: actionTypes.REMOVE_PERSON, personId: id})
+    }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Persons);
