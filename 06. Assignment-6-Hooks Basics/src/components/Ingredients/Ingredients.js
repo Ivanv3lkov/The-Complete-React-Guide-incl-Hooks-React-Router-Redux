cc9f0ab771372c4b1eee10f669fffe0d1e660f4e
ivanv3lkov <ivanv3lkov@gmail.com>
2020-12-28 10:56:57 +0200

    Assignment

--- 06. Assignment-6-Hooks Basics/src/components/Ingredients/Ingredients.js
+++ 06. Assignment-6-Hooks Basics/src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -12,42 +12,57 @@ const Ingredients = () => {
       .then(response => response.json())
       .then(responseData => {
         const loadedIngredients = [];
-        for (let key in responseData) {
+        for (const key in responseData) {
           loadedIngredients.push({
             id: key,
             title: responseData[key].title,
             amount: responseData[key].amount
-          })
+          });
         }
         setUserIngredients(loadedIngredients);
-      })
-  }, [])
+      });
+  }, []);
+
+  useEffect(() => {
+  }, [userIngredients]);
+
+  const filteredIngredientsHandler = useCallback(filteredIngredients => {
+    setUserIngredients(filteredIngredients);
+  }, []);
 
   const addIngredientHandler = ingredient => {
     fetch('https://hooks-practice-a8f14-default-rtdb.firebaseio.com/ingredients.json', {
       method: 'POST',
       body: JSON.stringify(ingredient),
-      headers: { 'Content-type': 'application/json' }
-    }).then(response => response.json())
+      headers: { 'Content-Type': 'application/json' }
+    })
+      .then(response => {
+        return response.json();
+      })
       .then(responseData => {
         setUserIngredients(prevIngredients => [
           ...prevIngredients,
           { id: responseData.name, ...ingredient }
         ]);
-      })
+      });
   };
 
   const removeIngredientHandler = ingredientId => {
-    setUserIngredients(prevIngredients => prevIngredients.filter(ingredient => ingredient.id !== ingredientId))
-  }
+    setUserIngredients(prevIngredients =>
+      prevIngredients.filter(ingredient => ingredient.id !== ingredientId)
+    );
+  };
 
   return (
     <div className="App">
       <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
-        <Search />
-        <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler} />
+        <Search onLoadIngredients={filteredIngredientsHandler} />
+        <IngredientList
+          ingredients={userIngredients}
+          onRemoveItem={removeIngredientHandler}
+        />
       </section>
     </div>
   );
